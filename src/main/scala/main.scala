620cfc5da9db7ceb2988064dff81ff5e5cec55ae
Lukas Rieger <lukasrieger07@gmail.com>
2024-01-31 02:19:31 +0100

    Test syntax dsl.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,9 +1,14 @@
 import Expression.Var
+import Program.Free
 
 @main
 def main(): Unit = {
   val test = dsl {
-    Var("a") |-> Var("b")
+    (Var("a") |-> Var("b")) ~~> (Var("c") |-> Var("d"))
+
+    Free(Var("a")) `;` Free(Var("a"))
+
+    (Var("a") |-> Var("b")) ~~> (Var("c") |-> Var("d")) ** (Var("e") |-> Var("f"))
   }
 
   println(test)
