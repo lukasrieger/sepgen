3cdbc0f8b39e3b0a8c7fbc3644d82e5723d0464e
Lukas Rieger <lukasrieger07@gmail.com>
2024-04-26 01:44:13 +0200

    braceless dsl.

--- src/main/scala/Examples.scala
+++ src/main/scala/Examples.scala
@@ -1,42 +1,32 @@
 
 
-object Examples {
-  
+object Examples:
   import pure.*
   import pure.PrgDsl.*
   import pure.Conversions.given_Conversion_Int_Lit
-  
-  
-  val listSum = program {
-    when(v("p") eq 0) {
+
+  val listSum = program:
+    when("p".v eq 0):
       returns (0)
-    } otherwise {
+    .otherwise:
       load("x".v, "p".v |-> "value")
       load("n".v, "p".v |-> "next")
       call("rec", List("n".v), "y".v)
-      returns("y".v + "x".v)
-    }
-  }
-    
-  val listLength = program {
-    when("p".v eq 0) {
+      returns ("y".v + "x".v)
+
+  val listLength = program:
+    when("p".v eq 0):
       returns (0)
-    } otherwise {
-      load("n".v, v("p") |-> "next")
-      call("rec", List(v("n")), v("y"))
-      returns (v("y") + 1)
-    }
-  }
+    .otherwise:
+      load("n".v, "p".v |-> "next")
+      call("rec", List("n".v), "y".v)
+      returns ("y".v + 1)
 
-
-  val listReverse: Program = program {
-    load(v("next"), v("curr") |-> "next")
-    when(v("next") eq 0) {
-      returns (v("curr"))
-    } otherwise {
+  val listReverse: Program = program:
+    load("next".v, "curr".v |-> "next")
+    when("next".v eq 0):
+      returns ("curr".v)
+    .otherwise:
       store ("prev".v) in ("curr".v |-> "next")
-      call("rec", List(v("curr"), v("next")), v("head"))
-      returns(v("head"))
-    }
-  }
-}
+      call("rec", List("curr".v, "next".v), "head".v)
+      returns ("head".v)
