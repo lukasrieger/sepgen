d5c98e59348db6614f4eed743b0e8aecb7582161
Lukas Rieger <lukasrieger07@gmail.com>
2024-04-26 01:45:12 +0200

    rename scope.

--- src/main/scala/pure/Program.scala
+++ src/main/scala/pure/Program.scala
@@ -21,12 +21,12 @@ package PrgDsl:
   case class StructPointer(variable: Var, field: String)
   case class PartialStore(arg: Expr)
 
-  trait PrgScope:
+  trait ProgramScope:
     val program: ListBuffer[Program] = ListBuffer()
 
 
-  def program(init: PrgScope ?=> Unit): Program =
-    given s: PrgScope = new PrgScope {}
+  def program(init: ProgramScope ?=> Unit): Program =
+    given s: ProgramScope = new ProgramScope {}
     val _ = init
     Program.Block(s.program.toList)
 
@@ -37,49 +37,49 @@ package PrgDsl:
     infix def |-> (field: String): StructPointer =
       StructPointer(v, field)
 
-  def assign(x: Var, expr: Expr)(using s: PrgScope): Unit =
+  def assign(x: Var, expr: Expr)(using s: ProgramScope): Unit =
     s.program += Program.Assign(x, expr)
 
-  def load(x: Var, pointer: Expr, field: Option[String] = None)(using s: PrgScope): Unit =
+  def load(x: Var, pointer: Expr, field: Option[String] = None)(using s: ProgramScope): Unit =
     s.program += Program.Load(x, pointer, field)
 
-  def load(x: Var, partial: StructPointer)(using s: PrgScope): Unit =
+  def load(x: Var, partial: StructPointer)(using s: ProgramScope): Unit =
     s.program += Program.Load(x, partial.variable, Some(partial.field))
 
-  def store(pointer: Expr, arg: Expr, field: Option[String] = None)(using s: PrgScope): Unit =
+  def store(pointer: Expr, arg: Expr, field: Option[String] = None)(using s: ProgramScope): Unit =
     s.program += Program.Store(pointer, arg, field)
 
-  def store(partial: StructPointer, arg: Expr)(using s: PrgScope): Unit =
+  def store(partial: StructPointer, arg: Expr)(using s: ProgramScope): Unit =
     s.program += Program.Store(partial.variable, arg, Some(partial.field))
 
   def store(arg: Expr): PartialStore =
     PartialStore(arg)
 
-  def when(test: Expr)(ifTrue: PrgScope ?=> Unit): PartialWhen =
-    given subScope: PrgScope = new PrgScope {}
+  def when(test: Expr)(ifTrue: ProgramScope ?=> Unit): PartialWhen =
+    given subScope: ProgramScope = new ProgramScope {}
     val _ = ifTrue
     if subScope.program.size == 1 then
       PartialWhen(test, subScope.program.head)
     else
       PartialWhen(test, Program.Block(subScope.program.toList))
 
-  def call(name: Name, args: List[Var], rt: Var)(using s: PrgScope): Unit =
+  def call(name: Name, args: List[Var], rt: Var)(using s: ProgramScope): Unit =
     s.program += Program.Call(name, args, rt)
 
-  def call(name: String, args: List[Var], rt: Var)(using s: PrgScope): Unit =
+  def call(name: String, args: List[Var], rt: Var)(using s: ProgramScope): Unit =
     s.program += Program.Call(Name(name), args, rt)
 
-  def returns(ret: Expr)(using s: PrgScope): Unit =
+  def returns(ret: Expr)(using s: ProgramScope): Unit =
     s.program += Program.Return(ret)
 
 
   extension (partial: PartialStore)
-    infix def in (pointer: Expr)(using s: PrgScope) = store(pointer, partial.arg, None)
-    infix def in (pointer: StructPointer)(using s: PrgScope) = store(pointer, partial.arg)
+    infix def in (pointer: Expr)(using s: ProgramScope) = store(pointer, partial.arg, None)
+    infix def in (pointer: StructPointer)(using s: ProgramScope) = store(pointer, partial.arg)
 
   extension (when: PartialWhen)
-    infix def otherwise(ifFalse: PrgScope ?=> Unit)(using s: PrgScope): Unit =
-      given subScope: PrgScope = new PrgScope {}
+    infix def otherwise(ifFalse: ProgramScope ?=> Unit)(using s: ProgramScope): Unit =
+      given subScope: ProgramScope = new ProgramScope {}
       val _ = ifFalse
       if subScope.program.size == 1 then
         s.program += Program.If(when.cond, when.ifTrue, subScope.program.head)
